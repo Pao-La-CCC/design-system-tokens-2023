3a64fd090a429a41cec7a991295727372919fff4
LisaAu <au.lisa08@gmail.com>
2023-02-16 16:30:01 +0100

    add PhraseLink

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/molecules/Button/Button'
+import { Button } from './components/molecules/Button/Button';
 import Input from './components/atoms/Input/Input';
-import { TextField } from './components/atoms/TextField/TextField';
+import TextField from './components/atoms/TextField/TextField';
+import PhraseLink from './components/molecules/PhraseLink/PhraseLink';
+
+
+let tag  = ['paragraphText' , 'headingText', 'standardText' ];
+let variant = [ 'niv1' , 'niv2', 'niv3' , 'niv4' , 'niv5', 'niv6'];
+let sizeElement = ['simple' , 'medium' , 'large' , 'extraLarge'];
 
 function App() {
-
-  let tag  = ['paragraphText' , 'headingText', 'standardText' ];
-  let variant = [ 'niv1' , 'niv2', 'niv3' , 'niv4' , 'niv5', 'niv6'];
-  let sizeElement = ['simple' , 'medium' , 'large' , 'extraLarge'];
 
   return (
     <div className="App">
@@ -16,6 +18,11 @@ function App() {
       <Input className="form-control" type="text" placeholder="rechercher" />
       <Button primary size="small" label=" Continuer" />
       <Button primary size="small" label="Envoyer" />
+      <PhraseLink 
+        text="Don't have an account?" 
+        linkText="Sign up" 
+        to="/signup"
+      />
     </div>
   );
 }
--- src/components/atoms/Links/Link.js
+++ src/components/atoms/Links/Link.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Link = ({ to, children, className }) => {
   return (
-    <RouterLink to={to} className={className}>
-      {children}
-    </RouterLink>
+    <BrowserRouter>
+      <RouterLink to={to} className={className}>
+        {children}
+      </RouterLink>
+    </BrowserRouter>
   );
 }
 
