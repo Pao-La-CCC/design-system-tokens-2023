1e27287ba564282e1749f4937b39bdee9205c437
jeseduardopi <jeseduardopi@gmail.com>
2023-02-16 11:52:24 +0100

    add storybook config with stories for button and input files

--- src/components/molecules/Button/Button.jsx
+++ src/components/molecules/Button/Button.jsx
@@ -1,36 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './button.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.scss";
 
-// import React from 'react'
+// TODO: fd
 
-// const Button = ({className,children,name,onClick,type,value}) => {
-//   return (
-//     <button
-//         className={className}
-//         name={name}
-//         onClick={onClick}
-//         type={type}
-//         value={value}
-//     >
-//         {children}
-
-//     </button>
-//   )
-// }
-
-// export default Button ;
-
-/**
- * Primary UI component for user interaction
- */
-
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({
+  primary,
+  label,
+  size,
+  backgroundColor,
+  onClick,
+  ...props
+}) => {
+  const mode = primary ? "btn-bg--primary" : "btn-bg--secondary";
+  
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
@@ -38,6 +27,9 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
     </button>
   );
 };
+
+
+
 
 Button.propTypes = {
   /**
@@ -51,7 +43,7 @@ Button.propTypes = {
   /**
    * How large should the button be?
    */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
   /**
    * Button contents
    */
@@ -65,6 +57,6 @@ Button.propTypes = {
 Button.defaultProps = {
   backgroundColor: null,
   primary: false,
-  size: 'medium',
+  size: "medium",
   onClick: undefined,
 };
